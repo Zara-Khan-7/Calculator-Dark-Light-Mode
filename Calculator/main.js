7cab529729bffa851ec4e7f2da9943041f1978c3
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-09-30 02:10:50 +0500

    Add files via upload

---
+++ Calculator/main.js
@@ -0,0 +1,9 @@
+// Select all the elements with the ID 'calc' and find their child 'span' elements
+var keys = document.querySelectorAll('#calc span');
+
+// Define an array of operators
+var operators = ['+', '-', 'x', '÷'];
+
+// Initialize a flag to track if a decimal point has been added
+var decimalAdded = false;
+
